9c7150a27a8350bf666ce6edf4bcb97bf556fa6a
MinhHieuIntershipTomosia <tt.hieu.vu@tomosia.com>
2023-04-03 18:01:57 +0700

    pratices project 03/04/2023

---
+++ Design_FrontEnd_WebThankCard/assets/js/select-option-thanksCard.js
@@ -0,0 +1,44 @@
+try {
+    const thanksCard  = document.getElementById('thanksCard--selected')
+    let lstBtnSelect = document.getElementsByClassName('newThanksCard__item');
+    for (const item of lstBtnSelect) {
+        item.addEventListener('click', function(){
+            thanksCard.innerHTML = ''
+            const itemsId = item.id;
+            switch(itemsId){
+                case '1':
+                    thanksCard.innerHTML = `<div class="thanksCard--bg thanksCardWendding">
+                    <div class="thanksCardWendding__container">
+                      <p class="inputCustom thanksCardWendding__title" role="textbox" contenteditable></p>
+                      <h1 class="inputCustom thanksCardWendding__Content" role="textbox" contenteditable></h1>
+                      <p class="inputCustom thanksCardWendding__description" role="textbox" contenteditable></p>
+                    </div>
+                  </div>`
+                    break;
+                case '2':
+                    thanksCard.innerHTML=`<div class="thanksCard--bg thanksCardHelp">
+                    <div class="thanksCardHelp__container">
+                      <h1 class="inputCustom thanksCardHelp__title" role="textbox" contenteditable></h1>
+                      <p class="inputCustom thanksCardHelp__description" role="textbox" contenteditable></p>
+                      <p class="inputCustom thanksCardHelp__underwrite" role="textbox" contenteditable></p>
+                      </p>
+                    </div>
+                    <div style="clear: both;"></div>
+                  </div>`
+                    break;
+                case '3':
+                    thanksCard.innerHTML = `<div class="thanksCard--bg thanksCardCristmast">
+                    <div class="thanksCardCristmast__container">
+                      <p class="inputCustom thanksCardCristmast__title" role="textbox" contenteditable></p>
+                      <h1 class="inputCustom thanksCardCristmast__Content" role="textbox" contenteditable></h1>
+                      <p class="inputCustom thanksCardCristmast__description" role="textbox" contenteditable></p>
+                    </div>
+                  </div>`
+                    break;
+            }
+        })
+    }
+    document.querySelector('')
+} catch (error) {
+    console.log(error)
+}
