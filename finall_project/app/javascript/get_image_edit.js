debac92f706e5b9bf3e90c8dbbc2d372cb444d25
MinhHieuIntershipTomosia <tt.hieu.vu@tomosia.com>
2023-04-19 17:43:16 +0700

    perform file upload function

--- finall_project/app/javascript/get_image_edit.js
+++ finall_project/app/javascript/get_image_edit.js
@@ -5,24 +5,33 @@ const uploadInputs = document.querySelectorAll('#thanksCard__image input.customF
 
 imagePreviews.forEach((imagePreview, index) => {
   const uploadInput = uploadInputs[index];
+  imagePreview.addEventListener('click', function () {
+    uploadInput.click();
+    uploadInput.addEventListener('change', () => {
+      const file = uploadInput.files[0];
+      const reader = new FileReader();
 
-  uploadInput.addEventListener('change', () => {
-    const file = uploadInput.files[0];
-    const reader = new FileReader();
+      reader.onload = function () {
+        imagePreview.src = reader.result;
+      }
 
-    reader.onload = function () {
-      imagePreview.src = reader.result;
-    }
-
-    reader.readAsDataURL(file);
-    lstimage();
-  }); 
+      reader.readAsDataURL(file);
+      lstimage();
+    });
+  })
 });
 
 function lstimage() {
   const files = [];
+  const lstimage_edit = [];
   uploadInputs.forEach(element => {
     const fileList = element.files;
+    if (fileList.length > 0) {
+      str = element.id
+      const vt = str.substring(str.indexOf("-") + 1);
+      lstimage_edit.push(vt)
+    }
+
     for (let i = 0; i < fileList.length; i++) {
       files.push(fileList[i])
     }
@@ -35,6 +44,8 @@ function lstimage() {
 
   const fileList = dataTransfer.files;
   document.querySelector('input#lstimage').files = fileList;
+  document.querySelector('input#lstimage-edit').value = lstimage_edit;
+
 }
 
 lstimage();
