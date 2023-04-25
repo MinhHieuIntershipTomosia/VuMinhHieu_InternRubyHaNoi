5991feea76bc270a358c2b81515dc6b8bd7bb171
MinhHieuIntershipTomosia <tt.hieu.vu@tomosia.com>
2023-04-25 17:10:06 +0700

    fixed some issues

--- finall_project/app/helpers/thanks_card_helper.rb
+++ finall_project/app/helpers/thanks_card_helper.rb
@@ -1,57 +1,66 @@
+# frozen_string_literal: true
+
+# The ThanksCardHelper module contains methods displayed thankscard.
 module ThanksCardHelper
+  # anho anho
   def test_lst_userreceiver
-    if (params[:thanks_card][:user_receiver_id].nil? || params[:thanks_card][:user_receiver_id].empty?)
-      flash[:danger] = "No has User receiver"
-      redirect_to sendnew_url
-    end
+    return unless params[:thanks_card][:user_receiver_id].present?
+
+    flash[:danger] = 'No has User receiver'
+    redirect_to sendnew_url
   end
 
   def lst_name_userreceiver(thanks_card)
-    if !thanks_card.nil?
-      users_receiver = thanks_card.users_receiver
-      user_receiver_names = users_receiver.map { |item| item.user.full_name }
-      users_receiver_string = user_receiver_names.join(", ")
-    end
+    return unless thanks_card
+
+    users_receiver = thanks_card.users_receiver
+    user_receiver_names = users_receiver.map { |item| item.user.full_name }
+    user_receiver_names.join(', ')
   end
 
-  def get_top_one_send
-    if ThanksCard.all.count > 0
-      lst_thankscard = ThanksCard.where(delete_status: false)
-      get_top_one(lst_thankscard)
-    end
+  def top_one_send
+    return unless ThanksCard.all.count.negative?
+
+    lst_thankscard = ThanksCard.where(delete_status: false)
+    top_one(lst_thankscard)
   end
 
-  def get_top_one_receiver
-    if UsersReceiver.all.count > 0
-      lst_user_receiver = UsersReceiver.where(delete_status: false, users_receivers_delete: false)
-      get_top_one(lst_user_receiver)
-    end
+  def top_one_receiver
+    lst_user_receiver = UsersReceiver.where(delete_status: false, users_receivers_delete: false)
+    return UsersReceiver.all.count.negative? unless lst_user_receiver.exists?
+
+    top_one(lst_user_receiver)
   end
 
-  def get_top_one(lst)
-    if !lst.nil?
-      hash_count = Hash.new(0)
-      lst.each do |items|
-        hash_count[items.user_id] += 1
-      end
+  def top_one(lst)
+    return if lst.nil?
 
-      sort_count = hash_count.sort_by { |k, v| v }
-
-      max_value = sort_count.last[1]
-
-      lst_max_value = sort_count.select { |k, v| v == max_value }
-      @lst_user = lst_max_value.map { |k, v| User.find(k) }
+    hash_count = Hash.new(0)
+    lst.each do |items|
+      hash_count[items.user_id] += 1
     end
+
+    sort_count = hash_count.sort_by { |_k, v| v }
+
+    max_value = sort_count.last[1]
+
+    lst_max_value = sort_count.select { |_k, v| v == max_value }
+    @lst_user = lst_max_value.map { |k, _v| User.find(k) }
   end
 
   def edit_image
-    if !params[:thanks_card][:edit].nil?
-      arr = params[:thanks_card][:edit].split(",")
-      arr.each_with_index do |items, index|
-        @image = @thankscard.image.find(items.to_i)
-        @image.purge
-        @thankscard.image.attach(params[:thanks_card][:image][index + 1])
-      end
+    return unless params.dig(:thanks_card, :edit).present?
+
+    image_ids = params[:thanks_card][:edit].split(', ')
+    image_params = params[:thanks_card][:image]
+    update_images(image_ids, image_params)
+  end
+
+  def update_images(ids, params)
+    ids.each_with_index do |id, index|
+      image = @thankscard.image.find(id.to_i)
+      image.purge
+      image.attach(params[index + 1])
     end
   end
 end
--- finall_project/app/javascript/create_new_image.js
+++ finall_project/app/javascript/create_new_image.js
@@ -1,25 +1,131 @@
 //= require jquery
-$('#btn-add-image').on('click', function () {
-  $('#customFile').click();
+// $(document).ready(function () {
+//   let status = false;
+//   $('#customFile').on('change', function () {
+//     if (status) {
+//       return;
+//     }
+//     status = true;
+
+//     const file = this.files[0];
+//     const reader = new FileReader();
+//     reader.onload = function () {
+//       $('#thanksCard__image-list').prepend(`
+//         <div class="image-add-item col-3 list-inline-item">
+//           <div>
+//             <div class="mb-4 d-flex justify-content-center">
+//               <img class="h-124 w-100" src="${reader.result}">
+//             </div>
+//             <div class="d-flex justify-content-center">
+//               <button type="button" class="btn-close" aria-label="Close"></button>
+//             </div>
+//           </div>
+//         </div>
+//       `);
+//     };
+//     reader.readAsDataURL(file);
+//     $(this).val(null);
+//     status = false;
+//   });
+
+//   $('#btn-add-image').on('click', function () {
+//     if (status) {
+//       return;
+//     }
+//     status = true;
+//     console.log($('#customFile').files) 
+//     $('#customFile').click();
+//     status = false;
+//   });
+
+// });
+
+$(document).ready(function () {
+  let status = false;
+
+  $('#btn-add-image').on('click', function () {
+    if (status) {
+      return;
+    }
+    status = true;
+
+    $('#customFile').one('click', function () {
+      status = false;
+    });
+  });
+
+  $(document).on('click', '.btn-close', function () {
+    const items = $(this).closest('.image-add-item');
+    const lst = $('.image-add-item.col-3.list-inline-item');
+    delete_image(lst);
+    items.remove();
+  });
+
+  $('#customFile').on('change', function () {
+    // xử lý sự kiện tạo ảnh
+    const file = this.files[0];
+    const reader = new FileReader();
+    let newItem = null;
+    reader.onload = function () {
+      newItem = `<div class="image-add-item col-3 list-inline-item">
+                  <div>
+                    <div class="mb-4 d-flex justify-content-center">
+                      <img class="h-124 w-100" src="${reader.result}">
+                    </div>
+                    <div class="d-flex justify-content-center">
+                      <button type="button" class="btn-close" aria-label="Close"></button>
+                    </div>
+                  </div>
+                </div> `
+      $('#thanksCard__image-list').prepend(newItem);
+    };
+    reader.readAsDataURL(file);
+    $(this).val(null);
+  });
+
+
+
 });
 
-$('#customFile').on('change', function () {
-  const file = this.files[0];
-  const reader = new FileReader();
-  reader.onload = function () {
-    $('#thanksCard__image-list').prepend(`
-      <div class="image-add-item col-3 list-inline-item">
-        <div>
-          <div class="mb-4 d-flex justify-content-center">
-            <img class="h-124 w-100" src="${reader.result}">
-          </div>
-          <div class="d-flex justify-content-center">
-            <button type="button" class="btn-close" aria-label="Close"></button>
-          </div>
-        </div>
-      </div>
-    `);
-  };
-  reader.readAsDataURL(file);
-  $('#customFile').val('');
-});
+
+const items = $('.image-add-item.col-3.list-inline-item');
+
+function beginvt() {
+  const items = $('.image-add-item.col-3.list-inline-item');
+  for (let i = 0; i < items.length; i++) {
+    if (!$(items[i]).is(':hidden')) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+function delete_image(lst) {
+  const begin = beginvt();
+  const end = begin + 2;
+  if (begin == 0 && end < lst.length) {
+    lst.eq(end).next().show();
+  } else if (begin > 0 && end == lst.length) {
+    lst.eq(begin).prev().show();
+  } else if (begin > 0 && end < lst.length) {
+    lst.eq(begin).prev().show();
+  }
+}
+
+function display(items) {
+  const perv = $('.control.prev')
+  const next = $('.control.next')
+  const begin = beginvt();
+  const end = begin + 2;
+  if (begin == 0) {
+    perv.hide();
+  } else if (begin > 0) {
+    perv.show();
+  }
+
+  if (next == items.length) {
+    next.hide();
+  } else if (next < items.length) {
+    next.show();
+  }
+}
